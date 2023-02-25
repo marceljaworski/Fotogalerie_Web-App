a1c5ceb2895e612c6c2ad7a9dfaeca49a06b551e
Marcelearn <jaworskimarcel@gmail.com>
2023-02-25 14:01:37 +0100

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./views/Home";
+import Albums from "./views/Albums";
+import Contact from "./views/Contact";
+import NoPage from "./views/NoPage";
 
 function App() {
 
 
   return (
     <div className="App">
-       <h1>My Foto Album</h1>
+    <BrowserRouter>
+     
+      <Routes>
+        <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+          <Route path="albums" element={<Albums />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
+
     </div>
   )
 }

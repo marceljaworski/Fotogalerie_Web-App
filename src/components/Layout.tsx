b5d93ec740fbb2084e0faf3301c050d4d4ee00ac
Marcelearn <jaworskimarcel@gmail.com>
2023-02-26 18:04:03 +0100

    uiMaterial's menu- icons

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,9 @@
-import { NavLink, Outlet, Link } from "react-router-dom";
+import { NavLink, Outlet} from "react-router-dom";
 import "./Layout.css"
+import GitHubIcon from '@mui/icons-material/GitHub';
+import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
+import LoginIcon from '@mui/icons-material/Login';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 
 const Layout = () => {
   return (
@@ -16,11 +20,18 @@ const Layout = () => {
             <NavLink to="/signup" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Sign Up</NavLink>
           </li>
           <li>
-            <NavLink to="/login" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Log In</NavLink>
+            <NavLink to="/login" className={({ isActive }) => (isActive ? 'active' : 'inactive')}><LoginIcon></LoginIcon></NavLink>
           </li>
           <li className="grow"></li>
-          <li>LikedIn</li>
-          <li>GitHub</li>
+          <li>
+          <NavLink to="/login" className={({ isActive }) => (isActive ? 'active' : 'inactive')}><AddAPhotoIcon></AddAPhotoIcon></NavLink>
+          </li>
+          <li>
+            <NavLink to="https://github.com/marceljaworski" className={({ isActive }) => (isActive ? 'active' : 'inactive')}><GitHubIcon></GitHubIcon></NavLink>
+            </li>
+          <li>
+          <NavLink to="/login" className={({ isActive }) => (isActive ? 'active' : 'inactive')}><AlternateEmailIcon></AlternateEmailIcon></NavLink>
+            </li>
         </ul>
       </nav>
 
